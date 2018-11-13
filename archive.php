0b539987f9505330388c2e8eb8f98d0500a97be7
Alvin Novian <alvinnovian883@gmail.com>
2018-11-13 16:27:18 +0700

    add archive

--- archive.php
+++ archive.php
@@ -8,46 +8,64 @@
  */
 
 get_header();
+
+        $titleText = '';
+        $detail = '';
+
+        // Define what this archieve refers to
+        if(is_category()){
+            // Category
+            $titleText = 'Category';
+            $detail = single_cat_title('',false);
+        }else if(is_tag()){
+            // Tag
+            $titleText = 'Tag';
+            $detail = single_cat_title('',false);
+        }else if(is_author()){
+            // Author
+            $titleText = 'Author';
+            $detail = get_the_author();
+        }else if(is_day()){
+            // Day
+            $titleText = 'Daily';
+            $detail = get_the_date();
+        }else if(is_month()){
+            // Month
+            $titleText = 'Monthly';
+            $detail = get_the_date('F Y');
+        }else if(is_year()){
+            // Year
+            $titleText = 'Yearly';
+            $detail = get_the_date('Y');
+        }
+?>
+	<div id="mini-header">
+		<div class="sub-header">
+			<div class="subtitle"><?php echo $titleText; ?> Archive</div>
+			<div class="title"><?php echo $detail; ?></div>
+			<div class="subtitle"><?php echo 'Found ' . $wp_query->found_posts . ' items'; ?></div>
+		</div>
+	</div>	
+<?php
+
+    if(have_posts()){ ?>
+        <div class="card-columns">
+            <?php
+                while (have_posts()) {
+                    the_post();
+                    get_template_part('template-parts/content-search');
+                }
+            ?>
+        </div>
+    <?php
+    } else {
+        get_template_part('404.php');
+    }
+	
+	// get_template_part('template-parts/landingpage/footer');
+
+	//get_sidebar();
+
+	get_footer();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
--- functions.php
+++ functions.php
@@ -92,10 +92,10 @@ function jaime_widgets_init() {
 		'name'          => esc_html__( 'Sidebar', 'jaime' ),
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'jaime' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_widget' => '',
+		'after_widget'  => '',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
 	) );
 }
 add_action( 'widgets_init', 'jaime_widgets_init' );
