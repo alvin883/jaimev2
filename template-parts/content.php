2214b58568fa208c9b98dad5d5161422d0167128
Alvin Novian <alvinnovian883@gmail.com>
2018-11-14 11:16:32 +0700

    ...

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,29 +10,29 @@
 ?>
 <div class="al-page">
 	<div class="header <?php if(!has_post_thumbnail()){echo 'no-thumb';}?>">
-	<?php if(has_post_thumbnail()){ ?>
-		<div class="thumbnail" style="background-image: url('<?php the_post_thumbnail_url(); ?>')">
+		<div class="thumbnail" style="background-image: url('<?php if(has_post_thumbnail()){ the_post_thumbnail_url(); } ?>')">
 			<div class="centered_content">
-				<div class="category">
+				<?php
+					$categories = get_the_category();
+					if($categories){
+				?>
+					<div class="category">
 					<?php
-						$categories = get_the_category();
-						if($categories){
 								$iteration = 0;
 								foreach ($categories as $category) {
 									if($iteration != 0){echo ' - ';}
 									echo $category->cat_name;
 									$iteration ++;
 								}
-						} 
 					?>
-				</div>
+					</div>
+				<?php } ?>
 				<div class="title"><?php the_title(); ?></div>
 				<div class="detail">
 					by <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a> <span class="divider">|</span> <?php the_time('d F Y'); ?>
 				</div>
 			</div>
 		</div>
-	<?php } ?>
 	</div>
 	<div class="wrapper">
 		<div class="content-wrapper">
