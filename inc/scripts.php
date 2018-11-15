6be0a0c5ca0dbe7d689d412e0330a72ddef8f3d6
Alvin Novian <alvinnovian883@gmail.com>
2018-11-15 11:45:47 +0700

    change header to wp_header

--- inc/scripts.php
+++ inc/scripts.php
@@ -4,17 +4,21 @@
  * Enqueue scripts and styles.
  */
 function jaime_scripts() {
-    // Add `style.css`
-    wp_enqueue_style( 'jaime-style', get_stylesheet_uri() );
 
-    // Add `js/thisscript.js`
+    // `js/thisscript.js`
     wp_enqueue_script( 'thisscript', get_template_directory_uri() . '/js/thisscript.js', array('jquery') );
 
-    // Add `js/get_instagram.js`
+    // `js/get_instagram.js`
     wp_enqueue_script( 'get_instagram', get_template_directory_uri() . '/js/get_instagram.js', array('jquery') );
 
-    // Add slick-carousel
+    // slick-carousel
     wp_enqueue_script( 'slick-carousel', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick.min.js', array('jquery') );
+
+    // Fontawesome
+    wp_enqueue_script( 'fontawesome', '//use.fontawesome.com/releases/v5.0.1/js/all.js', array(), '5.0.1' );
+    
+    // `style.css`
+    wp_enqueue_style( 'jaime-style', get_stylesheet_uri() );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
